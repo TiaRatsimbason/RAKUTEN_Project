7ecdb5517ae11b9c897c5dca2b94a676cce085e6
ElsaMack <elsa.makouri@yahoo.fr>
2024-09-07 22:45:16 +0200

    Modif et réorga de l'API: ajout de la gestion des modèles, optimisation des logs et des temps d'exécution, et création de la documentation API. Modification des chemins de données locaux et amélioration des scripts d'importation.

--- src/data/import_raw_data.py
+++ src/data/import_raw_data.py
@@ -1,75 +1,67 @@
-import requests
 import os
+import shutil
 import logging
 from check_structure import check_existing_file, check_existing_folder
+import time  # Pour le suivi du temps d'exécution
 
-
-def import_raw_data(raw_data_relative_path, filenames, bucket_folder_url):
-    """import filenames from bucket_folder_url in raw_data_relative_path"""
+def import_raw_data(raw_data_relative_path, filenames, local_folder_path):
+    """Import filenames from local_folder_path in raw_data_relative_path"""
     if check_existing_folder(raw_data_relative_path):
-        os.makedirs(raw_data_relative_path)
-    # download all the files
+        os.makedirs(raw_data_relative_path, exist_ok=True)
+
+    # Limiter à 100 fichiers pour accélérer l'exécution
+    filenames = filenames[:100]
+
+    # Copier les fichiers un par un
+    start_time = time.time()  # Démarrer le chronomètre
     for filename in filenames:
-        input_file = os.path.join(bucket_folder_url, filename)
+        input_file = os.path.join(local_folder_path, filename)
         output_file = os.path.join(raw_data_relative_path, filename)
-        if check_existing_file(output_file):
-            object_url = input_file
-            print(f"downloading {input_file} as {os.path.basename(output_file)}")
-            response = requests.get(object_url)
-            if response.status_code == 200:
-                # Process the response content as needed
-                content = (
-                    response.content
-                )  # Utilisez response.content pour les fichiers binaires
-                with open(output_file, "wb") as file:
-                    file.write(content)
-            else:
-                print(f"Error accessing the object {input_file}:", response.status_code)
 
-    # Téléchargez le dossier 'img_train'
-    img_train_folder_url = os.path.join(bucket_folder_url, "image_train/")
-    img_train_local_path = os.path.join(raw_data_relative_path, "image_train/")
-    if check_existing_folder(img_train_local_path):
-        os.makedirs(img_train_local_path)
-
-    try:
-        response = requests.get(img_train_folder_url)
-        if response.status_code == 200:
-            file_list = response.text.splitlines()
-            for img_url in file_list:
-                img_filename = os.path.basename(img_url)
-                output_file = os.path.join(img_train_local_path, img_filename)
-                if check_existing_file(output_file):
-                    print(f"downloading {img_url} as {img_filename}")
-                    img_response = requests.get(img_url)
-                    if img_response.status_code == 200:
-                        with open(output_file, "wb") as img_file:
-                            img_file.write(img_response.content)
-                    else:
-                        print(f"Error downloading {img_url}:", img_response.status_code)
+        if not os.path.exists(output_file):
+            print(f"Copying {input_file} as {os.path.basename(output_file)}")
+            shutil.copy(input_file, output_file)
         else:
-            print(
-                f"Error accessing the object list {img_train_folder_url}:",
-                response.status_code,
-            )
-    except Exception as e:
-        # old code : nonlocalprint(f"An error occurred: {str(e)}")
-        print(f"An error occurred: {str(e)}")
+            print(f"File {output_file} already exists, skipping.")
+
+    logging.info(f"Copie des fichiers terminée en {time.time() - start_time} secondes")
+
+    # Copier le dossier 'image_train'
+    img_train_folder = os.path.join(local_folder_path, "image_train/")
+    img_train_local_path = os.path.join(raw_data_relative_path, "image_train/")
+    
+    if check_existing_folder(img_train_local_path):
+        os.makedirs(img_train_local_path, exist_ok=True)
+
+    start_time = time.time()  # Recommencer le chronomètre pour la copie des images
+    if os.path.exists(img_train_folder):
+        img_filenames = os.listdir(img_train_folder)[:100]  # Limiter à 100 images
+
+        for img_filename in img_filenames:
+            input_image = os.path.join(img_train_folder, img_filename)
+            output_image = os.path.join(img_train_local_path, img_filename)
+            if not os.path.exists(output_image):
+                print(f"Copying {input_image} to {output_image}")
+                shutil.copy(input_image, output_image)
+            else:
+                print(f"Image {output_image} already exists, skipping.")
+    else:
+        print(f"Folder {img_train_folder} does not exist")
+
+    logging.info(f"Copie des images terminée en {time.time() - start_time} secondes")
 
 
 def main(
-    raw_data_relative_path="./data/raw",
+    raw_data_relative_path="C:/Users/Elsa/Documents/datascientest/projet_rakuten/juin24cmlops_rakuten_2/data/raw",
     filenames=["X_test_update.csv", "X_train_update.csv", "Y_train_CVw08PX.csv"],
-    bucket_folder_url="https://mlops-project-db.s3.eu-west-1.amazonaws.com/classification_e-commerce/",
+    local_folder_path="C:/Users/Elsa/Documents/datascientest/projet_rakuten/juin24cmlops_rakuten_2/data/preprocessed"
 ):
-    """Upload data from AWS s3 in ./data/raw"""
-    import_raw_data(raw_data_relative_path, filenames, bucket_folder_url)
+    """Copy data from local paths"""
+    import_raw_data(raw_data_relative_path, filenames, local_folder_path)
     logger = logging.getLogger(__name__)
     logger.info("making raw data set")
-
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     main()
