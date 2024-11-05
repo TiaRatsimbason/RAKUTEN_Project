948a4454842c13de43f334018ab2b638ce9fc6ab
Yaomingvini <vincentlouisonyan@gmail.com>
2024-11-05 20:39:48 +0100

    Modification pour corriger les droits dans le conteneur airflow

--- src/api/app.py
+++ src/api/app.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.api.routes import model
-
+from prometheus_fastapi_instrumentator import Instrumentator
 # Local/application-specific imports
 
 
@@ -14,6 +14,7 @@ app = FastAPI()
 
 API_URL = "/api"
 
+Instrumentator().instrument(app).expose(app)
 # MongoDB connection
 MONGODB_URI = os.getenv("MONGODB_URI")
 client = AsyncIOMotorClient(MONGODB_URI)
